f0803f1315270f29d6074e8312a8f468ddf5094e
adrisj7 <adrisj7@gmail.com>
2015-12-15 17:06:29 -0500

    Made functions spiffy

--- UnboundedArray.java
+++ UnboundedArray.java
@@ -1,30 +1,40 @@
 class UnboundedArray {
-	private Object[] array;
+/* UnboundedArray.java
+* This is basically us trying to make ArrayList, but it's gonna be better.
+* We promise.
+* Somebody replace this comment with a better description
+*
+*/
+
+	private Object[] array = new array[0];
 	private int length = 0;
+
 	
-	public UnboundedArray () {
-		
-	}
 	private Object[] toNewArray (Object[] oldArray, int length) {
 		/*
-		/ For pushing an array into a new array of a different size.
-		/ This is used for either adding a new element to the array, OR
-		/ decreasing the size
+		 For pushing an array into a new array of a different size.
+		 This is used for either adding a new element to the array, OR
+		 decreasing the size
 		*/
+		
 		Object[] newArray = new Object[length];
 		for(int i = 0; i < oldArray.length; i++) {
 			if (oldArray[i] != null) { // This cuts off, for decreasing the size of array.
 				newArray[i] = oldArray[i];
 			}
 		}
+		
+		return newArray;
 	}
 	
 	
 	
 	public void add(Object o, int index) {
+		if (index >= length) {
+			array = t
+		}else {
 		
+		}
 		
 	}
 }
-
-
